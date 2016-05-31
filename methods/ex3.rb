654cf80ecf61304a9d4f2369b3dd30ecadd77021
TzuYuan <tzuyuanliu0830@gmail.com>
2016-05-31 14:00:36 +0800

    method 3

---
+++ methods/ex3.rb
@@ -0,0 +1,11 @@
+# def greeting(name)
+#   "Hello, " + name + ". How are you doing?"
+# end
+
+# puts greeting("Bob")
+
+def multiply(number1, number2)
+  number1 * number2
+end
+
+puts multiply(4, 2)
