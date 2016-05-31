a6eac3ed86e6a3717e1b88328a36d9a08d8ac51a
TzuYuan <tzuyuanliu0830@gmail.com>
2016-05-31 14:17:16 +0800

    loops iterators 3

--- loops_iterators/ex3.rb
+++ loops_iterators/ex3.rb
@@ -1,14 +1,9 @@
-# evaluate_num.rb
+top_five_games = ["mario brothers",
+                  "excite bike",
+                  "ring king",
+                  "castlevania",
+                  "double dragon"]
 
-puts "Please enter a number between 0 and 100."
-number = gets.chomp.to_i
-
-if number < 0
-  puts "You can't enter a negative number!"
-elsif number <= 50
-  puts "#{number} is between 0 and 50"
-elsif number <= 100
-  puts "#{number} is between 51 and 100"
-else
-  puts "#{number} is above 100"
+top_five_games.each_with_index do | game, index |
+  puts "#{index + 1}. #{game}"
 end
