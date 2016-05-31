9de17d09044797f20ddc9586d264669b603ec473
TzuYuan <tzuyuanliu0830@gmail.com>
2016-05-31 13:46:03 +0800

    basic 3

--- basic.rb
+++ basic.rb
@@ -7,3 +7,15 @@ hundreds = 4936 % 1000 / 100
 tens = 4936 % 1000 % 100 / 10
 ones = 4936 % 1000 %100 % 10
 
+
+movies = { jaws: 1975,
+           anchorman: 2004,
+           man_of_steel: 2013,
+           a_beautiful_mind: 2001,
+           the_evil_dead: 1981 }
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
